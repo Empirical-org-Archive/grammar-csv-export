45c9053f1486e266d7b399bb76871b1e95d4091f
RyanNovas <ryan@quill.org>
2015-12-03 10:25:24 -0500

    added concept level 0

--- parser.rb
+++ parser.rb
@@ -7,27 +7,30 @@ class Parser
 
   def initialize
     @file = File.read('quillgrammar-concepts-export.json')
-    @csv_arrays = [['Concept Level 1', 'CCSS Standard',	'Question UID',	'Answers', 'Prompt']]
+    @csv_arrays = [['Concept Level 0', 'Concept Level 1', 'CCSS Standard',	'Question UID',	'Answers', 'Prompt']]
     @questions_hash = JSON.parse(@file)
-    @missing_fields = ["-Jzw0qjO5owyFPUAwDGK", "-Jzw0qjLNEeZAERlRksx", "-Jzw0qjO5owyFPUAwDGM", "-K2nXnevRx0EgXW07mzg"]
+    @missing_fields = ["-Jzw0qjO5owyFPUAwDGK", "-Jzw0qjLNEeZAERlRksx", "-Jzw0qjO5owyFPUAwDGM", "-K2nXnevRx0EgXW07mzg", "-K2iV539jnRtCyoWKh3N"]
   end
 
   # -Jzw0qjO5owyFPUAwDGK -- "Meanwhile"
   # -Jzw0qjO5owyFPUAwDGM -- "Meanwhile"
   # '-K2nXnevRx0EgXW07mzg' --  no questions
   # -Jzw0qjLNEeZAERlRksx -- correct messy sentences
+  # -K2iV539jnRtCyoWKh3N" -- 'Colons in lists' is missing concept level 0 
 
 
   def json_to_array
     @questions_hash.each do |concept|
       unless @missing_fields.include?(concept.first)
         ccss = concept[1]["standard"]["name"]
+        concept_level_0 = concept[1]['concept_level_0']['name']
         concept_level_1 = concept[1]['concept_level_1']['name']
+        puts concept_level_0
         concept[1]["questions"].each do |question|
           question_uid = question.first[1..-1]
           answers = question[1]['answers']
           prompt = question[1]['prompt']
-          @csv_arrays.push([concept_level_1, ccss, question_uid, answers, prompt])
+          @csv_arrays.push([concept_level_0, concept_level_1, ccss, question_uid, answers, prompt])
         end
       end
     end
